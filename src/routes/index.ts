5b7e59b31ba5eaef2b446bf161217cbd42d0da70
Gustavo Gorgônio <ggorgon03@gmail.com>
2023-09-15 16:36:50 -0300

    add specification routes at the app routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,11 @@ import { Router } from "express";
 
 import "../shared/container"; // Load tsyringe containers  // ! Want to remove this import
 import { categoriesRoutes } from "./categories.routes";
-// import { specificationRoutes } from "./specification.routes";
+import { specificationRoutes } from "./specification.routes";
 
 const router = Router();
 
 router.use("/categories", categoriesRoutes);
-// router.use("/specifications", specificationRoutes);
+router.use("/specifications", specificationRoutes);
 
 export { router };
