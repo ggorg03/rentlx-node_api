745216b39042f0624645039281f107704fc8e8cd
Gustavo Gorgônio <ggorgon03@gmail.com>
2023-09-15 16:37:31 -0300

    add Spcification Repository injection

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,8 +2,15 @@ import { container } from "tsyringe";
 
 import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository";
 import { CategoriesRepository } from "../../modules/cars/repositories/CategoriesRepository";
+import { ISpecificationRepository } from "../../modules/cars/repositories/ISpecificationRepository";
+import { SpecificationRepository } from "../../modules/cars/repositories/SpecificationRepository";
 
 container.registerSingleton<ICategoriesRepository>(
   "CategoriesRepository",
   CategoriesRepository,
+);
+
+container.registerSingleton<ISpecificationRepository>(
+  "SpecificationRepository",
+  SpecificationRepository,
 );
